d655cfeffaa08606f5be9c55f215c85cc239755f
The73756 <the73756the@gmail.com>
2023-05-23 21:56:40 +0900

    Upd: complete basic theme switching functionality

--- src/shared/ui/theme-switcher/theme-switcher.tsx
+++ src/shared/ui/theme-switcher/theme-switcher.tsx
@@ -1,17 +1,18 @@
 import { cls } from "@/shared/lib";
 import { useTheme } from "@/app/providers/theme-provider";
 import { Icon } from "@/shared/ui/icon/icon";
+import { Theme } from "@/app/providers/theme-provider/lib";
 
 interface ThemeSwitcherProps {
   className?: string;
 }
 
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
-  const { toggleTheme } = useTheme();
+  const { toggleTheme, theme } = useTheme();
 
   return (
-    <button className={cls("btn", {}, [className])}>
-      <Icon name="theme/moon" className="" onClick={toggleTheme} />
+    <button className={cls("btn-circle btn text-xl", {}, [className])} onClick={toggleTheme}>
+      {theme === Theme.DARK ? <Icon name="theme/moon" /> : <Icon name="theme/sun" />}
     </button>
   );
 };
