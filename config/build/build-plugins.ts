a81ae9d63b73a1a91db2faac30ac131848ed95fb
The73756 <the73756the@gmail.com>
2023-06-03 18:38:22 +0900

    Update: add hot-module-replacement in webpack

--- config/build/build-plugins.ts
+++ config/build/build-plugins.ts
@@ -2,6 +2,7 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import svg from "@neodx/svg/webpack";
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
@@ -26,5 +27,7 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: isDev,
     }),
+    new ReactRefreshWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ];
 };
