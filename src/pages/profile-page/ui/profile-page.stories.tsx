47455aab241673990c7b020108fa22fd42dd5020
The73756 <the73756the@gmail.com>
2023-07-05 14:30:25 +0900

    DX: fix storybook

--- src/pages/profile-page/ui/profile-page.stories.tsx
+++ src/pages/profile-page/ui/profile-page.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { StoreDecorator } from '@/shared/config/storybook'
+import { Country, Currency } from '@/shared/constants/common'
 import ProfilePage from './profile-page'
 
 const meta: Meta<typeof ProfilePage> = {
@@ -9,4 +11,23 @@ const meta: Meta<typeof ProfilePage> = {
 export default meta
 type Story = StoryObj<typeof ProfilePage>
 
-export const Basic: Story = {}
+export const Basic: Story = {
+  decorators: [
+    StoreDecorator({
+      profile: {
+        data: {
+          first: 'The',
+          lastname: '73756',
+          age: 10,
+          currency: Currency.RUB,
+          country: Country.Russia,
+          city: 'Moscow',
+          username: 'admin',
+          avatar: 'https://avatars.githubusercontent.com/u/94830363?v=4',
+        },
+        isLoading: false,
+        readonly: true,
+      },
+    }),
+  ],
+}
