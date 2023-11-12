4cc58b3ac2ac1edaf5eef365495cb967117d7aea
The73756 <the73756the@gmail.com>
2023-11-12 17:23:24 +0900

    Update: add article types

---
+++ src/entities/article/model/types/article.ts
@@ -0,0 +1,48 @@
+export enum ArticleBlockType {
+  TEXT = 'TEXT',
+  CODE = 'CODE',
+  IMAGE = 'IMAGE',
+}
+
+export enum ArticleType {
+  'IT' = 'IT',
+  'SCIENCE' = 'SCIENCE',
+  'ECONOMIC' = 'ECONOMIC',
+}
+
+export interface ArticleBlockBase {
+  id: string
+  type: ArticleBlockType
+}
+
+export interface ArticleTextBlock extends ArticleBlockBase {
+  id: string
+  type: ArticleBlockType.TEXT
+  title?: string
+  paragraphs: string[]
+}
+export interface ArticleCodeBlock extends ArticleBlockBase {
+  id: string
+  type: ArticleBlockType.CODE
+  code: string
+}
+
+export interface ArticleImageBlock extends ArticleBlockBase {
+  id: string
+  type: ArticleBlockType.IMAGE
+  src: string
+  title: string
+}
+
+export type ArticleBlock = ArticleTextBlock | ArticleImageBlock | ArticleCodeBlock
+
+export interface Article {
+  id: string
+  title: string
+  subtitle: string
+  img: string
+  views: number
+  createdAt: string
+  type: ArticleType[]
+  blocks: ArticleBlockBase[]
+}
