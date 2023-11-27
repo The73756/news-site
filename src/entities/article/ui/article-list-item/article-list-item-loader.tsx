c064d02f4e1692484e3894c5b5da4bc09e2b1681
The73756 <the73756the@gmail.com>
2023-11-27 20:24:09 +0900

    Update: add skeleton for articles list

---
+++ src/entities/article/ui/article-list-item/article-list-item-loader.tsx
@@ -0,0 +1,68 @@
+import { memo } from 'react'
+import { ArticleView } from '@/entities/article/model'
+import { cls } from '@/shared/lib/class-names'
+import { Skeleton, SkeletonWrapper } from '@/shared/ui'
+
+interface ArticleListItemLoaderProps {
+  className?: string
+  view: ArticleView
+}
+
+export const ArticleListItemLoader = memo(({ className, view }: ArticleListItemLoaderProps) => {
+  if (view === ArticleView.TILE) {
+    return (
+      <SkeletonWrapper className={cls('card', {}, [className])}>
+        <Skeleton width="100%" height={250} isImage={true} />
+        <div className="card-body px-0">
+          <div className="flex flex-wrap items-center justify-between gap-2">
+            <Skeleton height={22} width={120} />
+            <Skeleton height={18} width={75} />
+          </div>
+          <div className="card-actions justify-between">
+            <Skeleton width={75} height={18} />
+            <Skeleton width={60} height={18} />
+          </div>
+        </div>
+      </SkeletonWrapper>
+    )
+  }
+
+  return (
+    <SkeletonWrapper className={cls('card', {}, [className])}>
+      <div className="card-body px-0">
+        <div className="flex flex-wrap items-center justify-between gap-2">
+          <div className="flex items-center gap-4">
+            <Skeleton
+              className="min-w-[40px]"
+              width={40}
+              height={40}
+              isImage={true}
+              border="100%"
+            />
+            <div className="flex items-center gap-2">
+              <Skeleton width={70} height={18} />
+              <Skeleton width={75} height={18} />
+            </div>
+          </div>
+          <Skeleton width={70} height={18} />
+        </div>
+        <div className="card-actions justify-between">
+          <Skeleton width={200} height={24} />
+          <Skeleton width={80} height={18} />
+        </div>
+      </div>
+      <Skeleton width="100%" height={250} isImage={true} />
+      <div className="card-body px-0">
+        <div className="mb-2 space-y-2">
+          <Skeleton width="100%" height={14} />
+          <Skeleton width="90%" height={14} />
+          <Skeleton width="85%" height={14} />
+          <Skeleton width="95%" height={14} />
+        </div>
+        <div className="card-actions justify-end">
+          <Skeleton width={130} height={36} />
+        </div>
+      </div>
+    </SkeletonWrapper>
+  )
+})
--- src/entities/article/ui/article-list/article-list.tsx
+++ src/entities/article/ui/article-list/article-list.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
+import { ArticleListItemLoader } from '@/entities/article/ui/article-list-item/article-list-item-loader'
 import { cls } from '@/shared/lib/class-names'
 import { Article, ArticleView } from '../../model'
 import { ArticleListItem } from '../article-list-item'
@@ -12,24 +13,34 @@ interface ArticleListProps {
 }
 
 export const ArticleList = memo(
-  ({ className, articles, view = ArticleView.LIST, isLoading }: ArticleListProps) => {
+  ({ className, articles, view = ArticleView.TILE, isLoading = false }: ArticleListProps) => {
     const { t } = useTranslation()
+    const containerClass = cls(
+      '',
+      {
+        'article-grid': view === ArticleView.TILE,
+        'article-list': view === ArticleView.LIST,
+      },
+      [className]
+    )
+
+    if (isLoading) {
+      return (
+        <div className={containerClass}>
+          {new Array(view === ArticleView.TILE ? 10 : 2).fill(0).map((_, index) => (
+            // eslint-disable-next-line react/no-array-index-key -- it's ok for loaders
+            <ArticleListItemLoader key={index} view={view} />
+          ))}
+        </div>
+      )
+    }
 
     const renderArticle = (article: Article) => {
       return <ArticleListItem key={article.id} article={article} view={view} />
     }
 
     return (
-      <div
-        className={cls(
-          '',
-          {
-            'article-grid': view === ArticleView.TILE,
-            'article-list': view === ArticleView.LIST,
-          },
-          [className]
-        )}
-      >
+      <div className={containerClass}>
         {articles.length > 0 ? articles.map(renderArticle) : <div>{t('Статей нет')}</div>}
       </div>
     )
